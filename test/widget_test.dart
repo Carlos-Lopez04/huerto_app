5139155656116c81459845ae4313370a9b70cfc3
Joel Alejandro Tejeda Estrada <joel.estrada78@uabc.edu.mx>
2026-09-07 23:25:54 -0700

    Arreglar test inicializando Firebase con mock platform interface

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,19 +1,57 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:huerto_app/firebase_options.dart';
 import 'package:huerto_app/main.dart';
 
+class _MockFirebasePlatform extends FirebasePlatform {
+  _MockFirebasePlatform(this._options);
+
+  final FirebaseOptions _options;
+  final List<FirebaseAppPlatform> _apps = [];
+
+  @override
+  List<FirebaseAppPlatform> get apps => List.unmodifiable(_apps);
+
+  @override
+  Future<FirebaseAppPlatform> initializeApp({
+    String? name,
+    FirebaseOptions? options,
+  }) async {
+    final appName = name ?? defaultFirebaseAppName;
+    final appOptions = options ?? _options;
+    final app = FirebaseAppPlatform(appName, appOptions);
+    _apps.add(app);
+    return app;
+  }
+
+  @override
+  FirebaseAppPlatform app([String name = defaultFirebaseAppName]) {
+    return _apps.firstWhere(
+      (a) => a.name == name,
+      orElse: () => throw FirebaseException(
+        plugin: 'core',
+        code: 'no-app',
+        message: "No Firebase App '$name' has been created.",
+      ),
+    );
+  }
+}
+
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    FirebasePlatform.instance = _MockFirebasePlatform(
+      DefaultFirebaseOptions.currentPlatform,
+    );
+    await Firebase.initializeApp();
+  });
+
   testWidgets('renders the login screen without Firebase setup',
       (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
     await tester.pumpAndSettle();
 
